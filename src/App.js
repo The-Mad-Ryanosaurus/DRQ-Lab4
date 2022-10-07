36f2d84f5cde7786710258f5794a23cae0f6a9e8
The-Mad-Ryanosaurus <ryanharte1997@gmail.com>
2022-10-07 12:30:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header } from './components/header';
+import { Content } from './components/content';
+import { Footer } from './components/footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* Nav bar set to dark */}
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="/">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                {/* Nav.Links setup to make new pages for navigation */}
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/read">Read</Nav.Link>
+                <Nav.Link href="/create">Create</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+
+          <Routes>
+            {/* Displaying the 3 different components (header, content, footer) in their respective paths */}
+            <Route path='/' element={<Content></Content>}></Route>
+            <Route path='/read' element={<Header></Header>}></Route>
+            <Route path='/create' element={<Footer></Footer>}></Route>
+          </Routes>
+
+          {/* <Header></Header>
+        <Content></Content>
+        <Footer></Footer> */}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
